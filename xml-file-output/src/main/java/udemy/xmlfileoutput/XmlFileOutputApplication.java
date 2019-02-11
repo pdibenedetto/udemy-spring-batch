8b62a1a6a64449d5671d01909e214c8d9d0fd9bd
streamlesswaveson <streamlesswaveson@gmail.com>
2019-02-11 04:41:20 -0800

    5-23 - writing xml output to files

---
+++ xml-file-output/src/main/java/udemy/xmlfileoutput/XmlFileOutputApplication.java
@@ -0,0 +1,16 @@
+package udemy.xmlfileoutput;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class XmlFileOutputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlFileOutputApplication.class, args);
+    }
+
+}
+
---
+++ xml-file-output/src/main/java/udemy/xmlfileoutput/config/JobConfig.java
@@ -0,0 +1,100 @@
+package udemy.xmlfileoutput.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.batch.item.xml.StaxEventItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+import udemy.xmlfileoutput.domain.Customer;
+import udemy.xmlfileoutput.domain.CustomerRowMapper;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+public class JobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+
+    public JobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Customer> pagingItemReader() {
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+
+        reader.setDataSource(this.dataSource);
+        reader.setFetchSize(10);
+        reader.setRowMapper(new CustomerRowMapper());
+
+        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+
+        sortKeys.put("id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+
+        reader.setQueryProvider(queryProvider);
+
+        return reader;
+    }
+
+    @Bean
+    public StaxEventItemWriter<Customer> customerItemWriter() throws Exception {
+        XStreamMarshaller xStreamMarshaller = new XStreamMarshaller();
+        Map<String,Class> alias = new HashMap<>();
+        alias.put("customer", Customer.class);
+        xStreamMarshaller.setAliases(alias);
+
+        StaxEventItemWriter<Customer> staxEventItemWriter = new StaxEventItemWriter<>();
+        staxEventItemWriter.setRootTagName("customers");
+        staxEventItemWriter.setMarshaller(xStreamMarshaller);
+
+        String customerOutputPath = File.createTempFile("customerOutput", ".xml").getAbsolutePath();
+        log.info("customerOutputPath={}", customerOutputPath);
+
+        staxEventItemWriter.setResource(new FileSystemResource(customerOutputPath));
+        staxEventItemWriter.afterPropertiesSet();
+
+        return staxEventItemWriter;
+    }
+
+
+    @Bean
+    public Step step() throws Exception {
+        return stepBuilderFactory.get("step")
+                .<Customer,Customer>chunk(10)
+                .reader(pagingItemReader())
+                .writer(customerItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Job job() throws Exception {
+        String jobName = String.format("xml-file-output-job-%d", new Date().getTime());
+        return jobBuilderFactory.get(jobName)
+                .start(step())
+                .build();
+    }
+}
